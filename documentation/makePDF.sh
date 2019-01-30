26b2a0c0f861217b3f4e99bb5ad4cf55faae4ac1
rmadar <romain.madar@gmail.com>
2019-01-30 18:57:28 +0100

    Update the way pandoc is used to put as many option into title.yaml file

--- documentation/makePDF.sh
+++ documentation/makePDF.sh
@@ -12,69 +12,17 @@ if [ $# -ne 2 ]; then
 fi
 
 
-# Font selection for PDF creation via LaTeX
-# =========================================
-#font="txfonts"
-#fontoption="varg"
-#addoption="-V "
-#fontsize="10"
-
-font="sourcesanspro"
-fontoption="default"
-addoption="-V sans-serif"
-fontsize="10"
-
-#font="cmbright"
-#fontoption=""
-#addoption="-V sans-serif"
-#fontsize="10"
-
-#font="gillius"
-#fontoption="default"
-#addoption="-V sans-serif"
-#fontsize="10"
-
-#font="lmodern"
-#fontoption=""
-#addoption=""
-#fontsize="10"
-
-#font="sourceserifpro"
-#fontoption="default"
-#addoption=""
-#fontsize="10"
-
-#font="merriweather"
-#fontoption="light"
-#addoption=""
-#fontsize="10"
-
-#font="CrimsonPro"
-#fontoption=""
-#addoption=""
-#fontsize="11"
-
-#font="ebgaramond"
-#fontoption=""
-#addoption=""
-#fontsize="10"
-
-
 # PDF via latex
 pandoc -N -s ${PANDOC_TEMPLATES}/cross_references.yaml ${1} -o ${2}\
        --template ${PANDOC_TEMPLATES}/document_template.tex \
-       --filter ${PANDOC_FILTERS}/pandoc-crossref\
-       --filter pandoc-citeproc\
-       --variable geometry="a4paper, top=3.2cm, bottom=3.2cm, left=2.7cm, right=2.7cm"\
-       -V linestretch="1.2" -V fontsize=${fontsize} -V documentclass="book"\
+       --filter pandoc-crossref --filter pandoc-citeproc\
        --highlight-style tango\
-       --toc --toc-depth=2 -V fancypage\
-       -V fontfamily="${font}" -V fontoption="${fontoption}" ${addoption}
+       --toc --toc-depth 2
 
-	  
 # Make HTML
 pandoc -N -s ${PANDOC_TEMPLATES}/cross_references.yaml ${1} -o ${2}.html\
        --mathjax --css ${PANDOC_TEMPLATES}/webstyle_template.css\
        --filter pandoc-crossref --filter pandoc-citeproc\
-       --csl ${PANDOC_TEMPLATES}/biblio_style.csl\
-       --highlight-style tango
+       --csl ${PANDOC_TEMPLATES}/biblio_style.csl
+       --highlight-style tango\
+       --toc --toc-depth 2
